f20162d31b8bb1945ca82e56856d8428c5a2f3e3
NAME-E1 <abdulsalamadejumo9@gmail.com>
2022-09-09 10:52:53 -0700

    quote commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
+include <stdio.h>
 include <unistd.h>
-
 /**
  * main - Print "and that piece of art is usefull" - Dora Korpar, 2015-10-19"
- * Return: Always 1
- *\
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is usefull\" -Dora Korpar, 2015-10-19\n", 59);
