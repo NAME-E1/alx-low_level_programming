5a71ee859ecdd2005736c411759905c2fee9928c
NAME-E1 <abdulsalamadejumo9@gmail.com>
2022-09-12 19:12:03 -0700

    comb three no commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int c, i;
+
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
-		{						
-			if (c < i)						
-			{					
-				putchar(c);			
-				putchar(i);		
-				if (c != '8' || (c == '8' && i != '9'))		
-				{			
-					putchar(',');	
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+				if (c != '8' || (c == '8' && i != '9'))
+				{	
+					putchar(',');
 					putchar(' ');
 				}
 			}
