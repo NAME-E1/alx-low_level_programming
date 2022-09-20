fb9489f3bb91eab0872e3129aa92935571661bac
NAME-E1 <abdulsalamadejumo9@gmail.com>
2022-09-19 17:54:59 -0700

    first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * Descricption - Resets the value fo a variable to 98
+ * reset_to_98 - Resets the value fo a variable to 98
+ * n: value of variable for changing pointer dereference
  * Return: Always 0 success.
  */
 
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,10 @@
 #include "main.h"
 
 /**
- * Description: This function swaps the value of two integers
+ * swap_int- This function swaps the value of two integers
+ * a: the poiter dereference for changing the value of variable a
+ * b: a pointer dereference for changing the value of the variable b
+ * int c: assigning the address of the variable a to c
  * Return 0 success.
  */
 
